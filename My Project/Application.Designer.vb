c246ffce4e676f150dc237723ca5c45c5f04fa6d
Scott Wegley <scbrwegley@protonmail.ch>
2024-02-13 14:08:45 -0800

    updated form name references

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.CSC300_CarSim.Form1
+            Me.MainForm = Global.CSC300_CarSim.frmCarSim
         End Sub
     End Class
 End Namespace
