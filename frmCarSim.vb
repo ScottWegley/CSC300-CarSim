c453d0d22a6e3905081cc0492ac7173fa56cdf5b
Keegan Lenz <Keeganlenz12@gmail.com>
2024-02-13 16:11:37 -0800

    Basic line speedometer prototype

--- frmCarSim.vb
+++ frmCarSim.vb
@@ -1,4 +1,6 @@
-﻿Public Class frmCarSim
+﻿Imports System.Xml.Schema
+
+Public Class frmCarSim
 
     ' These represent the decay of speed and rpm naturally, values to be refined later
     ' Application interval currently undecided
@@ -16,6 +18,13 @@
     ' Used for calculating how long the gas/brakes are being held so we can scale the modification with the held length
     Dim tmrBrakeHeld As Timer = New Timer()
     Dim tmrGasHeld As Timer = New Timer()
+
+    Dim length = 100
+    Dim angle = Math.PI
+    Dim start_x = 200
+    Dim start_y = 350
+    Dim end_x = start_x
+    Dim end_y = start_y
 
     ' This sub is where all configuration code should go.
     Private Sub frmCarSim_Load(sender As Object, e As EventArgs) Handles Me.Load
@@ -39,5 +48,25 @@
 
     Private Sub pbxGas_MouseUp(sender As Object, e As MouseEventArgs) Handles pbxGas.MouseUp
         tmrGasHeld.Stop()
+    End Sub
+
+    Private Sub pbxSpeed_Click(sender As Object, e As EventArgs) Handles pbxSpeed.Click
+
+    End Sub
+
+    Private Sub BtnDraw_Click(sender As Object, e As EventArgs) Handles BtnDraw.Click
+        Dim graphics As Graphics = Me.CreateGraphics
+        Dim pen As Pen
+
+        end_x = start_x + Convert.ToInt32(Math.Cos(angle) * length)
+        end_y = start_y + Convert.ToInt32(Math.Sin(angle) * length)
+
+        pen = New Pen(Brushes.DarkMagenta, 1)
+
+        graphics.DrawLine(pen, start_x, start_y, end_x, end_y)
+
+        If angle + 0.1 < Math.PI * 2 Then
+            angle = angle + 0.1
+        End If
     End Sub
 End Class
