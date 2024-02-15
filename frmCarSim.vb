c3ad53bd629d64eae3f0fadd25e0f6d8d63f3a37
Keegan Lenz <Keeganlenz12@gmail.com>
2024-02-15 15:29:41 -0800

    Continuous update of line

--- frmCarSim.vb
+++ frmCarSim.vb
@@ -25,12 +25,19 @@ Public Class frmCarSim
     Dim start_y = 350
     Dim end_x = start_x
     Dim end_y = start_y
+    Dim accelerating As Boolean = True
+
+    Private WithEvents timer As Timer
 
     ' This sub is where all configuration code should go.
     Private Sub frmCarSim_Load(sender As Object, e As EventArgs) Handles Me.Load
         ' Currently the brakes and gas trigger 5 times a second, or once every 200 milliseconds, subject to change
         tmrBrakeHeld.Interval = 200
         tmrGasHeld.Interval = 200
+
+        timer = New Timer()
+        timer.Interval = 100 ' Update interval in milliseconds (e.g., 100ms)
+        timer.Start()
     End Sub
 
 
@@ -54,19 +61,35 @@ Public Class frmCarSim
 
     End Sub
 
-    Private Sub BtnDraw_Click(sender As Object, e As EventArgs) Handles BtnDraw.Click
-        Dim graphics As Graphics = Me.CreateGraphics
-        Dim pen As Pen
-
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles timer.Tick
+        ' Update end coordinates
         end_x = start_x + Convert.ToInt32(Math.Cos(angle) * length)
         end_y = start_y + Convert.ToInt32(Math.Sin(angle) * length)
 
-        pen = New Pen(Brushes.DarkMagenta, 1)
-
-        graphics.DrawLine(pen, start_x, start_y, end_x, end_y)
-
-        If angle + 0.1 < Math.PI * 2 Then
-            angle = angle + 0.1
+        ' Update angle for next iteration
+        If accelerating Then
+            If angle + 0.1 < Math.PI * 2 Then
+                angle = angle + 0.1
+            Else
+                accelerating = False
+            End If
+        Else
+            If angle - 0.1 > Math.PI Then
+                angle = angle - 0.1
+            Else
+                accelerating = True
+            End If
         End If
+
+        ' Trigger the Paint event to redraw the form
+        Me.Invalidate()
+    End Sub
+
+    Private Sub frmCarSim_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        Dim graphics As Graphics = e.Graphics
+        Dim pen As Pen = New Pen(Brushes.DarkMagenta, 1)
+
+        ' Draw the line
+        graphics.DrawLine(pen, start_x, start_y, end_x, end_y)
     End Sub
 End Class
