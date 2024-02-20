b76cb6191453a632f7c73cbb26248e95ddb7c4a2
scottwegley <scottwegley@gmail.com>
2024-02-19 17:05:27 -0800

    hooked up speed needle to gas

--- frmCarSim.Designer.vb
+++ frmCarSim.Designer.vb
@@ -26,6 +26,7 @@ Partial Class frmCarSim
         Me.pbxBrake = New System.Windows.Forms.PictureBox()
         Me.pbxGas = New System.Windows.Forms.PictureBox()
         Me.pbxRpm = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         CType(Me.pbxSpeed, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.pbxBrake, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.pbxGas, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -75,11 +76,19 @@ Partial Class frmCarSim
         Me.pbxRpm.TabIndex = 3
         Me.pbxRpm.TabStop = False
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(22, 401)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 4
+        '
         'frmCarSim
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1142, 707)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.pbxRpm)
         Me.Controls.Add(Me.pbxGas)
         Me.Controls.Add(Me.pbxBrake)
@@ -91,6 +100,7 @@ Partial Class frmCarSim
         CType(Me.pbxGas, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.pbxRpm, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -98,4 +108,5 @@ Partial Class frmCarSim
     Friend WithEvents pbxBrake As PictureBox
     Friend WithEvents pbxGas As PictureBox
     Friend WithEvents pbxRpm As PictureBox
+    Friend WithEvents TextBox1 As TextBox
 End Class
