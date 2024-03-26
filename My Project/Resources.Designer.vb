6b3d42f42c8e58d7cccb1ada7098f25ce12bed57
Adam Cartozian <acartozian@callutheran.edu>
2024-03-26 14:47:28 -0700

    Fuel and temp gauge

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -323,6 +323,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property TemoAndFuelGauge() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("TemoAndFuelGauge", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property TurnSignalStockDown() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("TurnSignalStockDown", resourceCulture)
