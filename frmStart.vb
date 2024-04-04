d15163a8a31fc60c56520b2e4d924a148868a586
Scott Wegley <scbrwegley@protonmail.ch>
2024-04-04 15:46:24 -0700

    fixed merge issues, i am but a fool, exit button, menu bar for start screen

--- frmStart.vb
+++ frmStart.vb
@@ -1,3 +1,11 @@
 ﻿Public Class frmStart
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Dim frmCarSim As frmCarSim = New frmCarSim()
+        frmCarSim.Show()
+        Me.Close()
+    End Sub
 
+    Private Sub mnStartExit_Click(sender As Object, e As EventArgs) Handles mnStartExit.Click
+        Me.Close()
+    End Sub
 End Class
