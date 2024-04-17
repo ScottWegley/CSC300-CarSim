3ce24e3ae651a68bef5280e9eb3254f5f9b570f4
Adam Cartozian <acartozian@callutheran.edu>
2024-04-17 14:02:21 -0700

    Updated Start Form To make it Look Good

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -359,6 +359,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property StartBackground() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("StartBackground", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property SteeringWheel() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("SteeringWheel", resourceCulture)
