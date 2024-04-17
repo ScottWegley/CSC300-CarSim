45496a99b5ed06641ad1d18645d6d8d91e66608e
Scott Wegley <scbrwegley@protonmail.ch>
2024-04-17 14:28:25 -0600

    empty subs to play media for later

---
+++ Radio.vb
@@ -0,0 +1,35 @@
+﻿Public Class Radio
+    Private boolRadioOn As Boolean = False
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub toggleBooleanOn()
+        boolRadioOn = Not boolRadioOn
+    End Sub
+
+    Public Sub PlayMediaOne()
+
+    End Sub
+
+    Public Sub PlayMediaTwo()
+
+    End Sub
+
+    Public Sub PlayMediaThree()
+
+    End Sub
+
+    Public Sub PlayMediaFour()
+
+    End Sub
+
+    Public Sub PlayMediaFive()
+
+    End Sub
+
+    Public Sub PlayMediaSix()
+
+    End Sub
+End Class
