8b237f0da61ec2a0e6899647ec527183183cc1d4
Scott Wegley <scbrwegley@protonmail.ch>
2024-04-17 11:55:34 -0600

    basic clock class

--- Car.vb
+++ Car.vb
@@ -6,17 +6,19 @@ Public Class Car
     Private blinkers As Blinkers
     Private lights As Lights
     Private rpmSystem As RPMSystem
+    Private clock As Clock
 #End Region
 
     Public Function isOn()
         Return rpmSystem.isOn()
     End Function
 
-    Public Sub New(ByRef lblMPH_IN As Label, ByRef lblGear_IN As Label, ByRef TextBox2_IN As System.Windows.Forms.TextBox, ByRef TextBox3_IN As System.Windows.Forms.TextBox, ByRef TextBox4_IN As System.Windows.Forms.TextBox, ByRef SpeedGauge As PictureBox, ByRef RPMGauge As PictureBox, ByRef ParkingBrakeLight As PictureBox, ByRef DriveIndicator As Label, ByRef RightBlinker As PictureBox, ByRef LeftBlinker As PictureBox, ByRef TurnStockDefault As PictureBox, ByRef TurnStockDown As PictureBox, ByRef TurnStockUp As PictureBox, ByRef LowBeams As PictureBox, ByRef HighBeams As PictureBox, ByRef FogLights As PictureBox)
+    Public Sub New(ByRef lblMPH_IN As Label, ByRef lblGear_IN As Label, ByRef TextBox2_IN As System.Windows.Forms.TextBox, ByRef TextBox3_IN As System.Windows.Forms.TextBox, ByRef TextBox4_IN As System.Windows.Forms.TextBox, ByRef SpeedGauge As PictureBox, ByRef RPMGauge As PictureBox, ByRef ParkingBrakeLight As PictureBox, ByRef DriveIndicator As Label, ByRef RightBlinker As PictureBox, ByRef LeftBlinker As PictureBox, ByRef TurnStockDefault As PictureBox, ByRef TurnStockDown As PictureBox, ByRef TurnStockUp As PictureBox, ByRef LowBeams As PictureBox, ByRef HighBeams As PictureBox, ByRef FogLights As PictureBox, ByRef ClockBox As PictureBox)
 
         lights = New Lights(LowBeams, HighBeams, FogLights)
         blinkers = New Blinkers(RightBlinker, LeftBlinker, TurnStockDefault, TurnStockUp, TurnStockDown)
         rpmSystem = New RPMSystem(lblMPH_IN, lblGear_IN, TextBox2_IN, TextBox3_IN, TextBox4_IN, SpeedGauge, RPMGauge, ParkingBrakeLight, DriveIndicator)
+        clock = New Clock(ClockBox)
     End Sub
 
     Public Function getBlinkers() As Blinkers
---
+++ Clock.vb
@@ -0,0 +1,7 @@
+﻿Public Class Clock
+    Private pbxClock As PictureBox
+
+    Public Sub New(ByRef Clock As PictureBox)
+        pbxClock = Clock
+    End Sub
+End Class
